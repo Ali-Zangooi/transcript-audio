d91caed60aa35c1a75cf3f015ad920eaa9f960dc
Ali-Zangooi <95471375+Ali-Zangooi@users.noreply.github.com>
2021-12-03 14:36:14 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 
 VERSION = '0.1.0'
-PACKAGE_NAME = 'transcript-audio'
+PACKAGE_NAME = 'AudioTranscript'
 AUTHOR = 'Ali Zangooi'
 AUTHOR_EMAIL = 'seagu.ll@yahoo.com'
 URL = 'https://github.com/Ali-Zangooi/transcript-audio/tree/main'
