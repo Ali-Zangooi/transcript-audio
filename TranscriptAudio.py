8f50a4d297d82a20078586d263e6e17297b3be2c
Ali-Zangooi <95471375+Ali-Zangooi@users.noreply.github.com>
2021-12-03 13:34:52 +0330

    Update TranscriptAudio.py

--- TranscriptAudio.py
+++ TranscriptAudio.py
@@ -6,6 +6,12 @@ from scipy.io import wavfile
 import speech_recognition as sr
 
 def extract(fileExt):
+    """ Extracts file with given paramter
+    Parameters: 
+        file's extention
+    Returns:
+        string array of files which end with the given extention
+    """
     fileDir = os.getcwd()
 
     files = []
@@ -18,12 +24,22 @@ def extract(fileExt):
 
 
 def convert2wav(mp3s):
+    """ Creates a folder and converts .mp3 files into .wav ones into the folder
+    Parameters:
+        string array of mp3 files
+    """
     os.mkdir("output")
     for file in mp3s:
         subprocess.call(['ffmpeg', '-i', file,"output/"+ file[:-4] + ".wav"])
         
 
 def audioLen(audioName):
+    """ Calculates the length of a given .wav file
+    Parameters:
+        name of the audio file
+    Returns:
+        .wav file's length in seconds
+    """
     rate, data = wavfile.read(audioName)
     return (len(data) / rate)
 
